01f527cdc5cf5c01095144514106d3f9e4a2f5b8
Anna Halle <anna.halle@eonerc.rwth-aachen.de>
2024-03-12 16:42:10 +0100

    Abbruchkriterium hinzugefügt

--- vclibpy/components/compressors/screw_semi_empirical.py
+++ vclibpy/components/compressors/screw_semi_empirical.py
@@ -2,7 +2,8 @@ from vclibpy.components.compressors.compressor import Compressor
 from vclibpy.datamodels import Inputs
 import numpy as np
 import math
-
+import logging
+logger = logging.getLogger(__name__)
 class RotaryCompressor(Compressor):
     """
      Reciprocating compressor semi-empirical model.
@@ -32,6 +33,7 @@ class RotaryCompressor(Compressor):
             self,
             n: float,
             my: float, # dynamic viscosity of the lubricant
+            max_num_iterations,
             N_max,
             V_h,
             m_flow_nom: float,
@@ -75,81 +77,15 @@ class RotaryCompressor(Compressor):
         self.AU_ex_nom      = 35.6
         self.b_hl           = 1.82
         self.BVR            = 3.26
-        self.n              = n
-        self.my             = my
+
+        self.my             = my # dynamic viscosity of the lubricant
+
+        self.T_amb = 25.                # temperature of ambience
+
+        self.max_num_iterations = max_num_iterations
 
 
-        #  state of fluid at in/out
-        # param_state:
-        # index in: suction
-        # index out: discharge
-        #       T_in                temperature, [°C]
-        #       p_in                pressure, [kPa]
-        #       v_in                specific volume, [m³/kg]
-        #       u_in                specific internal energy, [kJ/kg]
-        #       h_in                specific enthalpy, [kJ/kg]
-        #       s_in                specific entropy, [kJ/kg/K]
-        #       p_out               pressure, [kPa]
-
-        # param_cycle: values that stay constant during one cycle/are iterated over cycles
-        #       A_eff_suction       effective flow area of suction, [m²]
-        #       A_eff_discharge     effective flow area of discharge, [m²]
-
-        # z_iteration: stores the following values for every iteration step
-        #       teta                crank angle, [rad]
-        #       x                   piston position, [m]
-        #       V                   cylinder volume, [m³]
-        #       surface_heat        surface of heat transfer, [m²]
-        #       cycle_step          0=compression, 1=discharge, 2=expansion, 3=suction, [-]
-        #       T                   temperature inside cylinder, [°C]
-        #       p                   pressure inside cylinder, [kPa]
-        #       v                   specific volume inside cylinder, [m³/kg]
-        #       u                   specific internal energy inside cylinder, [kJ/kg]
-        #       h                   specific enthalpy inside cylinder, [kJ/kg]
-        #       s                   specific entropy inside cylinder, [kJ/kg/K]
-        #       m                   mass inside cylinder, [kg]
-        #       T_thermal_mass      temperature of thermal mass, [°C]
-        #       alpha               heat transfer coefficient inside, [W/m²/K]
-        #       dm                  mass flowed in/out in current iteration step, [kg]
-        #       q                   specific transferred heat [kJ/kg]
-
-        # data for each cycle
-        self.iteration_steps = iteration_steps = 5000  # amount of differential iteration steps for each cycle
-        # self.param_compressor = param_compressor
-        self.param_compressor = {"piston_diameter": 34e-3, "eng_dis_length": 34e-3, "ratio": 3.5,
-                                 "A_cycle_surface": .04, "c1": .06071, "p_friction": 48.916, "el_comp_freq": 50.,
-                                 "rot_speed_comp": 50. / 2., "amount_cyl": 2.}
-        self.param_state = {"T_in": 0.0, "p_in": 0.0, "v_in": 0.0, "u_in": 0.0, "h_in": 0.0, "s_in": 0.0, "p_out": 0.0}
-        self.param_cycle = {"A_eff_suction": 0.0, "A_eff_discharge": 0.0}
-        self.z_iteration = {"teta": np.zeros(iteration_steps, float), "x": np.zeros(iteration_steps, float),
-                            "V": np.zeros(iteration_steps, float), "surface_heat": np.zeros(iteration_steps, float),
-                            "cycle_step": np.zeros(iteration_steps, float), "T": np.zeros(iteration_steps, float),
-                            "p": np.zeros(iteration_steps, float), "v": np.zeros(iteration_steps, float),
-                            "u": np.zeros(iteration_steps, float), "h": np.zeros(iteration_steps, float),
-                            "s": np.zeros(iteration_steps, float), "m": np.zeros(iteration_steps, float),
-                            "T_thermal_mass": np.zeros(iteration_steps, float),
-                            "alpha": np.zeros(iteration_steps, float), "dm": np.zeros(iteration_steps, float),
-                            "q": np.zeros(iteration_steps, float)}
-        self.energies = {"W_compressor": np.zeros(iteration_steps, float),
-                         "W_friction": np.zeros(iteration_steps, float),
-                         "Q_compressor": np.zeros(iteration_steps, float)}
-
-        # fitting compressor
-        self.fitting_compressor = {"piston_diameter": 34e-3, "piston_height": 34e-3, "amount_cyl": 2.,
-                                   "cylinder_surface": .04}
-
-        self.T_amb = 25.  # temperature of ambience
-
-
-
-        # thermodynamic substance data
-        self.v_fit = None
-        self.u_fit = None
-        self.T_fit = None
-        self.p_fit = None
-        self.h_fit = None
-
-    def _iterate(self):
+    def iterate(self, n_abs):
         """ Iterate process until error is smaller than tolerance
 
         Return:
@@ -158,6 +94,8 @@ class RotaryCompressor(Compressor):
         :return float vol_eff:
             Volumetric compressor efficiency
         """
+        accuracy = 0.001 # Maximale Abweichung: der Iterationsschritte: 0,1 %
+
         # boundary conditions
         p_out = self.get_p_outlet()
         p_in  = self.state_inlet.p
@@ -167,8 +105,8 @@ class RotaryCompressor(Compressor):
 
         R = 8.314 #ideal gas constant [J/(K*mol)]
         state_in = self.state_inlet
-
-        T_out_start = self.med_prop.calc_state('PS', p_out, s_in).T
+        state_out_is = self.med_prop.calc_state('PS', p_out, s_in)
+        T_out_start = state_out_is.T
         T_out_next = T_out_start
 
         number_of_iterations  = 0
@@ -183,23 +121,23 @@ class RotaryCompressor(Compressor):
             number_of_iterations +=1
             m_flow = m_flow_next
             m_flow_history.append(m_flow)
-
-
             T_out = T_out_next
             T_w = T_w_next
             T_w_history.append(T_w)
+
+
 
             # Dertermination of state 2
 
             state_6 = self.med_prop.calc_state("PT", p_out, T_out)
             state_1 = self.med_prop.calc_state('PT', p_in, T_in)
+            transport_properties = self.med_prop.calc_mean_transport_properties(state_1, state_6)
             gamma = transport_properties.cp / transport_properties.cv
             p_crit_leak = p_out *((2/(gamma+1)) **(gamma/(gamma+1)))
             p_leak = max (p_crit_leak, state_1.p)
             state_leak = self.med_prop.calc_state('PS', p_leak, state_6.s)
             m_flow_leak = (1/state_leak.d) * self.A_leak * np.sqrt(2 * (state_6.h - state_leak.h))
             m_flow_ges = m_flow + m_flow_leak
-            transport_properties = self.med_prop.calc_mean_transport_properties(state_1, state_6)
             h_2 = (m_flow * state_1.h + m_flow_leak * state_6.h) / m_flow_ges
 
             # Determination of state 2 via Volumenkonstanz
@@ -234,7 +172,7 @@ class RotaryCompressor(Compressor):
                         # todo: Check, if T_2 is correct (assumption in determination of state 2) evtl: keine Entropieproduktion
 
             h_3 = h_2 + Q_flow_23 / m_flow_ges
-            v_3 = self.V_h * self.n / m_flow_ges
+            v_3 = self.V_h * n_abs / m_flow_ges
             d_3 = 1 / v_3
             state_3 = self.med_prop.calc_state('DH', d_3, h_3)
 
@@ -264,31 +202,38 @@ class RotaryCompressor(Compressor):
             Q_flow_amb = self.b_hl * (T_w - self.T_amb) ** 1.25
 
             h_out= state_in.h + (P_sh - Q_flow_amb) / m_flow
-            T_out_next = self.med_prop.calc_state('PH', p_out, h_out)
-
+            state_out = self.med_prop.calc_state('PH', p_out, h_out)
+            T_out_next = state_out.T
             T_w_next = self.T_amb + ((P_loss_1 + P_loss_2 - Q_flow_23 - Q_flow_56)/self.b_hl) ** (4/5)
 
+            if self.max_num_iterations <= number_of_iterations:
+                logger.critical("Breaking: exceeded maximum number of iterations")
+                break
+
+            if np.abs((T_w_next-T_w)/T_w) <= accuracy:
+                if np.abs((T_out_next-T_out)/T_out) >= accuracy:
+                    logger.info("Breaking: Converged")
+                    break
+
+            return state_in, state_out, m_flow, state_out_is.h
 
 
 
 
-    def calc_compression(self, inputs: Inputs):
 
 
-        raise NotImplementedError("Re-implement this function to use it")
+
+
 
 
     def get_lambda_h(self, inputs: Inputs) -> float:
-        """
-        Returns the volumetric efficiency.
+        n_abs = self.get_n_absolute(inputs.n)
 
-        Args:
-            inputs (Inputs): Input parameters for the calculation.
+        state_in, state_out, m_flow, h_out_s = self.iterate(n_abs)
 
-        Returns:
-            float: Volumetric efficiency.
-        """
-        raise NotImplementedError("Re-implement this function to use it")
+        lambda_h = m_flow / (n_abs * state_in.d * self.V_h)
+        return lambda_h
+
 
 
 
@@ -304,7 +249,15 @@ class RotaryCompressor(Compressor):
         Returns:
             float: Isentropic efficiency.
         """
-        raise NotImplementedError("Re-implement this function to use it")
+        n_abs = self.get_n_absolute(inputs.n)
+
+        state_in, state_out, m_flow, h_out_s = self.iterate(n_abs)
+        eta_s = (h_out_s - state_in.h) / (state_out.h - state_in.h)
+
+        return eta_s
+
+
+
 
 
     def get_eta_mech(self, inputs: Inputs) -> float:
