f5454df96aba1b1161a397b0f68d89d804aabaa9
Anna Halle <anna.halle@eonerc.rwth-aachen.de>
2024-07-16 15:34:53 +0200

    Add class of OpenTenCoefficientCompressor
    Open compressors are driven by an external electric motor. Thus, mechanical power is given by 10 coefficient polynomials. TenCoefficientCompressor is adapted to polynomials calculating mechanical power

--- vclibpy/components/compressors/__init__.py
+++ vclibpy/components/compressors/__init__.py
@@ -1,4 +1,4 @@
 from .compressor import Compressor
 from .rotary import RotaryCompressor
-from .ten_coefficient import TenCoefficientCompressor, DataSheetCompressor
+from .ten_coefficient import TenCoefficientCompressor, DataSheetCompressor, OpenTenCoefficientCompressor
 from .constant_effectivness import ConstantEffectivenessCompressor
