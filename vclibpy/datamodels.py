0b5f56b421ce35c725a70ae7b38ee3d05afb92cd
fabian.wuellhorst <fabian.wuellhorst@eonerc.rwth-aachen.de>
2024-11-25 15:57:03 +0100

    Separate inputs to control, eva and con for a cleaner approach

--- vclibpy/datamodels.py
+++ vclibpy/datamodels.py
@@ -1,7 +1,7 @@
 """
 Module which contains datamodels which are used in this library.
 """
-
+import abc
 from dataclasses import dataclass
 from typing import Dict, Any
 from copy import deepcopy
@@ -30,6 +30,7 @@ class VariableContainer:
 
     This class enables dynamic addition of Variables.
     """
+
     def __init__(self):
         self._variables: Dict[str, Variable] = {}
 
@@ -154,97 +155,181 @@ class FlowsheetState(VariableContainer):
     """
 
 
-class Inputs(VariableContainer):
+class HeatExchangerInputs(VariableContainer):
     """
-    Class defining inputs to calculate the FlowsheetState.
+    Class defining inputs for a heat exchanger.
 
-    While the inputs are pre-defined, you may add further ones
-    using the `set` method.
+    Note that some inputs presuppose each other.
+    You may only specify values which follow this equation:
+    Q = m_flow * cp * abs(T_out - T_in) = m_flow * cp * dT
+
+    Moreover, the amount of required inputs depend on the
+    control inputs you specify.
+    For example, when providing dT_superheating, dT_subcooling, and n,
+    both at least two arguments must be free.
+    However, you always have to specify either T_in or T_out to define
+    the absolute temperature levels.
 
     Args:
-        n (float): Relative compressor speed between 0 and 1.
-        T_eva_in (float): Secondary side evaporator inlet temperature.
-        T_con_in (float): Secondary side condenser inlet temperature.
-        m_flow_eva (float): Secondary side evaporator mass flow rate.
-        m_flow_con (float): Secondary side condenser mass flow rate.
-        dT_eva_superheating (float): Super-heating after evaporator.
-        dT_con_subcooling (float): Subcooling after condenser.
-        T_ambient (float): Ambient temperature of the machine.
+        T_in (float):
+            Secondary side inlet temperature in [K].
+        T_out (float):
+            Secondary side outlet temperature in [K].
+        dT (float):
+            Secondary side temperature difference in between inlet and outlet[K].
+            May be provided instead of m_flow or both T_in and T_out.
+        m_flow (float):
+            Secondary side mass flow rate in [kg/s].
+            May be provided instead of m_flow or both T_in and T_out.
+        Q (float):
+            Heat flow rate in [W].
+            May be provided
+        T_ambient (float):
+            Ambient temperature of the heat exchanger in [K].
+            Only used if the heat exchnager models account for
+            heat losses.
     """
 
     def __init__(
             self,
-            n: float = None,
-            T_eva_in: float = None,
-            T_con_in: float = None,
-            T_con_out: float = None,
-            m_flow_eva: float = None,
-            m_flow_con: float = None,
-            dT_eva_superheating: float = None,
-            dT_con_subcooling: float = None,
+            T_in: float = None,
+            T_out: float = None,
+            dT: float = None,
+            m_flow: float = None,
             T_ambient: float = None
     ):
         """
-        Initializes an Inputs object with parameters representing external conditions
-        for the vapor compression cycle.
-
-        Note that some inputs presuppose each other. For example, you can not
-        specify both `T_con_in`, `T_con_out`, and `m_flow_con`,
-        as one needs to be free for the simulation.
-        The current algorithms require `m_flow_con`
-        and either `T_con_in` or `T_con_out`.
-
-        Args:
-            n (float): Relative compressor speed between 0 and 1 (unit: -).
-            T_eva_in (float): Secondary side evaporator inlet temperature (unit: K).
-            T_con_in (float): Secondary side condenser inlet temperature (unit: K).
-            T_con_out (float): Secondary side condenser inlet temperature (unit: K).
-            m_flow_eva (float): Secondary side evaporator mass flow rate (unit: kg/s).
-            m_flow_con (float): Secondary side condenser mass flow rate (unit: kg/s).
-            dT_eva_superheating (float): Super-heating after evaporator (unit: K).
-            dT_con_subcooling (float): Subcooling after condenser (unit: K).
-            T_ambient (float): Ambient temperature of the machine (unit: K).
+        Initializes object with parameters representing external conditions
+        for the heat exchanger.
         """
         super().__init__()
-        if T_con_out is not None and T_con_in is not None:
-            raise ValueError("You can either specify condenser inlet or "
-                             "outlet temperature, not both.")
+        # Handle over specified cases m_flow and dT
+        if T_in is None and T_out is None:
+            raise ValueError("You either have to specify T_in or T_out")
+        elif T_in is None and T_out is not None:
+            self.uses_outlet = True
+            if dT is not None:
+                T_in = T_out - dT
+                self.uses_inlet = True
+            else:
+                self.uses_inlet = False
+        elif T_in is not None and T_out is None:
+            self.uses_inlet = True
+            if dT is not None:
+                T_out = T_in + dT
+                self.uses_outlet = True
+            else:
+                self.uses_outlet = False
+        else:
+            self.uses_outlet = True
+            self.uses_inlet = True
+            if dT is None:
+                dT = T_out - T_in
+            else:
+                if dT != T_out - T_in:
+                    raise ValueError(f"Given {dT=} does not match {T_in=} and {T_out=}")
+        if m_flow is not None and dT is not None:
+            raise ValueError("You can either specify the temperature difference or m_flow, not both")
+        self.set(
+            name="T_in",
+            value=T_in,
+            unit="K",
+            description="Secondary side inlet temperature"
+        )
+        self.set(
+            name="T_out",
+            value=T_out,
+            unit="K",
+            description="Secondary side outlet temperature"
+        )
+        self.set(
+            name="dT",
+            value=dT,
+            unit="K",
+            description="Secondary side temperature difference"
+        )
+        self.set(
+            name="m_flow",
+            value=m_flow,
+            unit="kg/s",
+            description="Secondary side mass flow rate"
+        )
+        # Handle optional T_ambient
+        if T_ambient is None:
+            T_ambient = self.T
+        self.set(
+            name="T_ambient",
+            value=T_ambient,
+            unit="K",
+            description="Ambient temperature of machine"
+        )
 
+    @property
+    def T(self):
+        """Returns either T_in, T_out, or the mean, depending on what is specified."""
+        if self.uses_inlet and self.uses_outlet:
+            return (self.T_in + self.T_out) / 2
+        if self.uses_inlet:
+            return self.T_in
+        return self.T_out
+
+    def get_all_inputs(self, cp: float, Q: float):
+        # Case with both inlet and outlet
+        if self.uses_inlet and self.uses_outlet:
+            T_in = self.T_in
+            T_out = self.T_out
+            m_flow = Q / cp / self.dT
+        elif self.uses_inlet:
+            T_in = self.T_in
+            T_out = self.T_in + Q / cp / self.m_flow
+            m_flow = self.m_flow
+        else:  # self.uses_outlet is always True in this case
+            T_out = self.T_out
+            T_in = T_out - Q / cp / self.m_flow
+            m_flow = self.m_flow
+        dT = T_out - T_in
+        if m_flow is None:
+            raise ValueError("Provided inputs are insufficient to calculate m_flow")
+        return T_in, T_out, dT, m_flow
+
+
+class ControlInputs(VariableContainer, abc.ABC):
+    """
+    Abstract class defining inputs to control the
+    vapor compression machine.
+    """
+
+
+class RelativeCompressorSpeedControl(VariableContainer):
+    """
+    Class defining inputs to control the
+    vapor compression machine using the relative compressor
+    speed.
+    Furthermore, superheating and subcooling levels must be
+    specified.
+
+    Args:
+        n (float): Relative compressor speed between 0 and 1 (unit: -).
+        dT_eva_superheating (float): Super-heating after evaporator (unit: K).
+        dT_con_subcooling (float): Subcooling after condenser (unit: K).
+    """
+
+    def __init__(
+            self,
+            n: float,
+            dT_eva_superheating: float,
+            dT_con_subcooling: float,
+    ):
+        """
+        Initializes a ControlInputs object with parameters representing external conditions
+        to control the vapor compression cycle.
+        """
+        super().__init__()
         self.set(
             name="n",
             value=n,
             unit="-",
             description="Relative compressor speed"
-        )
-        self.set(
-            name="T_eva_in",
-            value=T_eva_in,
-            unit="K",
-            description="Secondary side evaporator inlet temperature"
-        )
-        self.set(
-            name="T_con_in",
-            value=T_con_in,
-            unit="K",
-            description="Secondary side condenser inlet temperature"
-        )
-        self.set(
-            name="T_con_out",
-            value=T_con_out,
-            unit="K",
-            description="Secondary side condenser outlet temperature"
-        )
-        self.set(
-            name="m_flow_con",
-            value=m_flow_con,
-            unit="kg/s",
-            description="Secondary side condenser mass flow rate"
-        )
-        self.set(
-            name="m_flow_eva",
-            value=m_flow_eva,
-            unit="kg/s",
-            description="Secondary side evaporator mass flow rate"
         )
         self.set(
             name="dT_eva_superheating",
@@ -258,27 +343,108 @@ class Inputs(VariableContainer):
             unit="K",
             description="Subcooling after condenser"
         )
-        if T_ambient is None:
-            T_ambient = T_eva_in
+
+
+class CondenserPowerControl(ControlInputs):
+    """
+    Class defining inputs to control the
+    vapor compression machine using the condenser heat flow rate.
+    Furthermore, superheating and subcooling levels must be
+    specified.
+
+    Args:
+        Q_con (float): Condenser heat flow rate (unit: W).
+        dT_eva_superheating (float): Super-heating after evaporator (unit: K).
+        dT_con_subcooling (float): Subcooling after condenser (unit: K).
+    """
+
+    def __init__(
+            self,
+            Q_con: float,
+            dT_eva_superheating: float,
+            dT_con_subcooling: float,
+    ):
+        """
+        Initializes a ControlInputs object with parameters representing external conditions
+        to control the vapor compression cycle.
+        """
+        super().__init__()
         self.set(
-            name="T_ambient",
-            value=T_ambient,
+            name="Q_con",
+            value=Q_con,
+            unit="-",
+            description="Relative compressor speed"
+        )
+        self.set(
+            name="dT_eva_superheating",
+            value=dT_eva_superheating,
             unit="K",
-            description="Ambient temperature of machine"
+            description="Super-heating after evaporator"
+        )
+        self.set(
+            name="dT_con_subcooling",
+            value=dT_con_subcooling,
+            unit="K",
+            description="Subcooling after condenser"
         )
 
-    @property
-    def T_con(self):
-        """Returns either T_con_in or T_con_out, depending on what is specifiec."""
-        if self.T_con_in is not None:
-            return self.T_con_in
-        return self.T_con_out
 
-    @property
-    def uses_condenser_inlet(self):
-        """Indicates if condenser inlet or outlet is specified"""
-        return self.T_con_in is not None
+class Inputs:
+    """
+    Class defining inputs to calculate the FlowsheetState.
+
+    Args:
+        control (ControlInputs):
+            Input parameters for control.
+            Depending on your choice of control inputs,
+            you may have to adjust the required values
+            in the heat exchanger inputs.
+            Also, based on the control setting, the steady state
+            calculation will use different algorithms.
+        evaporator (HeatExchangerInputs):
+            Input parameters for evaporator
+        condenser (HeatExchangerInputs):
+            Input parameters for condenser
+    """
+
+    def __init__(
+            self,
+            control: ControlInputs,
+            evaporator: HeatExchangerInputs,
+            condenser: HeatExchangerInputs
+    ):
+        """
+        Initializes an Inputs object with parameters representing external conditions
+        of the vapor compression cycle.
+        """
+        self.control = control
+        self.evaporator = evaporator
+        self.condenser = condenser
 
 
 if __name__ == '__main__':
-    print(Inputs().T_con_out)
+    print(HeatExchangerInputs(T_in=273.15, dT=5))
+    print("-----")
+    print(HeatExchangerInputs(T_out=273.15, dT=5))
+    print("-----")
+    print(HeatExchangerInputs(T_in=273.15, T_out=278.15, dT=5))
+    print("-----")
+    print(HeatExchangerInputs(T_in=273.15, m_flow=5))
+    print("-----")
+    print(HeatExchangerInputs(T_out=273.15, m_flow=5))
+    try:
+        HeatExchangerInputs(dT=5)
+    except ValueError as err:
+        print("-----")
+        print(err)
+    try:
+        HeatExchangerInputs(m_flow=5)
+    except ValueError as err:
+        print("-----")
+        print(err)
+    try:
+        HeatExchangerInputs(T_in=273.15, T_out=278.15, m_flow=5)
+    except ValueError as err:
+        print("-----")
+        print(err)
+    print(HeatExchangerInputs(T_out=273.15, dT=5).get_all_inputs(cp=4184, Q=1000))
