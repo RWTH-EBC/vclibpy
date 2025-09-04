6b80886c30d524e198aeb749d790c8fb134a5a53
Anna Halle <anna.halle@eonerc.rwth-aachen.de>
2025-09-04 16:57:26 +0200

    Version Bogdan

--- vclibpy/media/oil_mix_prop.py
+++ vclibpy/media/oil_mix_prop.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on 17.07.2025
+Created on 25.07.2025
 
 @author: Anna Halle, Roman Provolotskyi, Bogdan Bykov
 
@@ -9,6 +9,7 @@ Created on 17.07.2025
 
 import abc
 import logging
+import matlab.engine
 from vclibpy.media import ThermodynamicState, TransportProperties, OilProp, RefProp
 
 
@@ -18,181 +19,227 @@ logger = logging.getLogger(__name__)
 class OilMixProp(OilProp):
     """
     Class to connect to OilMixProp package.
-
-    Args:
-    :param string fluid_name:
-        Fluid name for RefProp use
-    :param list or None z:
-        Fluid composition. Should only be used, when a self-design mixture shall be used. Further information
-        see notes.
-        When you want to use a self-design mixture to as follows:
-        - Fluid-name needs to contain all component names within mixture: "R32.FLD|R125.FLD"
-        - z needs to be a list with molar fractions: [0.697, 0.303]
-        - Used example would be similar to R410A
-
-    :param string dll_path:
-        Specifier for the dll path used for RefProp,
-        e.g. dll_path='C:\\path_to_dll\\RefProp64.dll'.
-        If None, the `ref_prop_path` and function `get_dll_path` are
-        used to determine the dll path
-    :param boolean use_error_check:
-        Specifier whether errors and warnings shall be checked when calling RefProp or not
-    :param boolean use_warnings:
-        Specifier whether warnings shall be used
-    :param str ref_prop_path:
-        Path to RefProp package. Default is the ENV variable `RPPREFIX`.
-    :param bool copy_dll:
-        If True (not the default), a copy of the dll is created to enable simultaneous use of
-        multiple fluids in multiprocessing.
-    :param str copy_dll_directory:
-        If `copy_dll` is True, the DLL is copied to this directory.
-        If None (default), the current working directory is used.
-
-    Note:
-        - You need to install package ctREFPROP Package
-          https://github.com/usnistgov/REFPROP-wrappers/tree/master/wrappers/python
-        - In case you use a self-defined mixture: Entropy reference state might deviate from GUI!!
-
-    Functionality:
-        - It does not work to have multiple instances simultaneously. When calculating values the last fluid name
-            somehow will be used even though instance includes "new" name. Need to be fixed at some point.
-
-
-    How to use RefProp-Wrapper:
-    ---------------------------
-    1.) Create RefProp instance: rp = RefProp("R32")
-    2.) In case you want to calculate fluid properties (state variables) for a specific state: Use calc_state() function
-            Multiple inputs can be given (but you need to now two in order to define the values). For further
-            information see function header.
-    3.) Further get-Functions implemented
-        - get_gwp(): Global warming potential
-        - get_odp(): Ozone depletion potential
-        - get_safety(): Safety class
-        - get_mass_fraction(): Mass fractions of pure substances in fluid
-        - get_molar_mass(): Molar mass of fluid
-        - get_mol_fraction(): Mol fractions of pure substances in fluid
-        - get_comp_names(): Pure substances names in fluid
-        - get_longname(): Long name of fluid within RefProp
-        - get_critical_point(): Crit. Temperature and Pressure
-        - get_version(): Version of wrapper and of RefProp dll
-
-
-
     """
-    # General information
-    #
 
     __author__ = "Anna Halle"
 
     _fluid_mapper = {}
 
-    def __init__(self,
-                 fluid_name, lub_name, eos):
+    def __init__(self, fluid_name, lub_name, OilMixProp_path='D:\\OilMixProp-main'):
 
-
-        super().__init__(fluid_name=fluid_name, lub_name=lub_name, eos=eos)
-
+        super().__init__(fluid_name=fluid_name, lub_name=lub_name)
+        self.OilMixProp_path = OilMixProp_path
 
 
     def terminate(self):
         if self._delete_dll_path is not None:
             self._delete_dll()
 
+    def get_critical_point(self):
+        raise NotImplementedError("get_critical_point must be implemented.")
 
+    def get_molar_mass(self):
+        raise NotImplementedError("get_molar_mass must be implemented.")
+
+    @staticmethod
+    def get_field_safe(mat_struct, field):
+        """
+        Safely extracts a field from a MATLAB struct returned as a Python dictionary.
+        Converts matlab.double to float or list.
+        """
+        if not isinstance(mat_struct, dict):
+            print("[WARN] Result is not a dictionary.")
+            return None
+
+        if field not in mat_struct:
+            return None
+
+        val = mat_struct[field]
+
+        # Convert matlab.double to float or list
+        if isinstance(val, matlab.double):
+            flat = list(val._data)
+            if len(flat) == 1:
+                return float(flat[0])
+            return [float(x) for x in flat]
+
+        # Return other types directly
+        return val
+
+    @staticmethod
+    def translate_user_key_to_state(user_key: str) -> str:
+        """
+        Converts user input like 'PH' into internal format like 'P-H'.
+        Raises ValueError if the combination is not supported.
+        """
+
+        # Unterstützte Zustandskombinationen im Programm
+        supported_states = {
+            "TP": "T-P",  # Temperature, Pressure
+            "TD": "T-D",  # Temperature, Density
+            "TS": "T-S",  # Temperature, Entropy
+            "TH": "T-H",  # Temperature, Enthalpy
+            "TQ": "T-Q",  # Temperature, Vapor quality
+            "PD": "P-D",  # Pressure, Density
+            "PS": "P-S",  # Pressure, Entropy
+            "PH": "P-H",  # Pressure, Enthalpy
+            "PQ": "P-Q",  # Pressure, Vapor quality
+        }
+
+        # Benutzereingabe in Großbuchstaben umwandeln
+        key = user_key.upper()
+
+        # Überprüfen, ob die Kombination unterstützt wird
+        if key not in supported_states:
+            raise ValueError(f"Unsupported combination '{key}'.")
+
+        # Entsprechenden internen Zustandsschlüssel zurückgeben
+        return supported_states[key]
 
     def calc_state(self, mode: str, var1: float, var2: float, lub_frac: float):
-        """ Calculate state. Depending on mode, different function will be chosen. Input state variables need to be in
-        SI units!
-
-        Notes:
-        ------
-        1.) PT does not work when state might be within the two-phase region!
-        2.) Only functions for density are implemented. In case you know the specific volume instead use density
-                functions with inverse value!
-        3.) Quality can have values outside of 'physical' scope:
-                q = -998: Subcooled liquid
-                q = 998: Superheated vapor
-                q = 999: Supercritical state
-
-        Possible modes are currently:
-            - "PD": Pressure, Density
-            - "PH": Pressure, Enthalpy
-            - "PQ": Pressure, Quality
-            - "PS": Pressure, Entropy
-            - "PT": Pressure, Temperature
-            - "PU": Pressure, Internal Energy
-            - "TD": Temperature, Density
-            - "TH": Temperature, Enthalpy
-            - "TQ": Temperature, Quality
-            - "TS": Temperature, Entropy
-            - "TU": Temperature, Internal Energy
-            - "DH": Density, Enthalpy
-            - "DS": Density, Entropy
-            - "DU": Density, Internal Energy
-            - "HS": Enthalpy, Entropy
-
-        Parameters:
-        -----------
-        :param string mode:
-            Defines which input state variables are given (see possible modes above)
-        :param float var1:
-            Value of state variable 1 (first one in name) - use SI units!
-        :param float var2:
-            Value of state variable 2 (second one in name) - use SI units!
-        :param int kr:
-            phase flag (kr=1: lower density, kr=2: higher density)
-            relevant for "TH", "TS", "TU"
-
-        Return:
-        -------
-        :return ThermodynamicState state:
-            Thermodynamic state with state variables
         """
+    Calculates thermodynamic state and transport properties for a given input condition.
+
+    Parameters:
+    ----------
+    mode : str
+        Input variable mode
+        Supported two-parameter combinations for state calculation:
+
+        "TP": Temperature [K], Pressure [Pa]
+        "TD": Temperature [K], Density [kg/m³]
+        "TS": Temperature [K], Entropy [J/kg/K]
+        "TH": Temperature [K], Enthalpy [J/kg]
+        "TQ": Temperature [K], Vapor Quality [-]
+        "PD": Pressure [Pa], Density [kg/m³]
+        "PS": Pressure [Pa], Entropy [J/kg/K]
+        "PH": Pressure [Pa], Enthalpy [J/kg]
+        "PQ": Pressure [Pa], Vapor Quality [-]
+
+    var1 : float
+        Value of the first input variable (must be in SI units).
+    var2 : float
+        Value of the second input variable (must be in SI units).
+    lub_frac : float
+        Lubricant mass fraction (between 0 and 1).
+
+    Returns:
+    -------
+    state : ThermodynamicState
+        Contains the thermodynamic properties:
+            - p : Pressure [Pa]
+            - T : Temperature [K]
+            - u : Internal Energy [J/kg]
+            - h : Enthalpy [J/kg]
+            - s : Entropy [J/kg/K]
+            - d : Density [kg/m³]
+            - q : Vapor Quality [-]
+    props : TransportProperties
+        Contains the transport properties:
+            - lam : Thermal Conductivity [W/m/K]
+            - dyn_vis : Dynamic Viscosity [Pa·s]
+            - kin_vis : Kinematic Viscosity [m²/s]
+            - pr : Prandtl Number [-]
+            - cp : Specific Heat Capacity at Constant Pressure [J/kg/K]
+            - cv : Specific Heat Capacity at Constant Volume [J/kg/K]
+            - beta : Isobaric Thermal Expansion Coefficient [1/K]
+            - sur_ten : Surface Tension [N/m] (not available)
+            - ace_fac : Accommodation Factor [-] (not available)
+    """
 
         ref = self.fluid_name
         lub = self.lub_name
 
-        state = ThermodynamicState(p=p, T=T, u=u, h=h, s=s, d=d, q=q)
-        props = TransportProperties(lam=None,
-                                    dyn_vis=None,
-                                    kin_vis=None,
-                                    pr=None,
-                                    cp=None,
-                                    cv=None,
-                                    beta=None,
-                                    sur_ten=None,
-                                    ace_fac=None,
-                                    state=state)
-        self.props = props
-        return state
+        eng = matlab.engine.start_matlab()
+        logger.info("MATLAB engine started")
+
+        eng.cd(self.OilMixProp_path, nargout=0)
+        eng.addpath(self.OilMixProp_path, nargout=0)
+
+        refrigerants = [ref, lub]
+        mass_fracs = [1.0 - lub_frac, lub_frac]
+
+        if not 1 <= len(refrigerants) <= 3:
+            raise ValueError('Only 1 to 3 components are allowed.')
+
+        if len(mass_fracs) not in (1, 2):
+            raise ValueError('mass_fracs must contain 1 or 2 values.')
+
+        if len(mass_fracs) > len(refrigerants):
+            raise ValueError('Too many mass fractions for the number of components.')
+
+        if len(refrigerants) == 3 and len(mass_fracs) == 2:
+            mass_fracs.append(1.0 - sum(mass_fracs))
+
+        if any(mf < 0 for mf in mass_fracs) or sum(mass_fracs) > 1 + 1e-6:
+            raise ValueError('Invalid mass fractions.')
+
+        # EOS Auswahl: Einer Davon Auswählen: 'PR', 'SRK', 'PTV', 'YFR'
+        eos_choice = 'YFR'
+
+        # T P Guess(einfach als 0 lassen, falls keine gute Einschätzung nicht bekannt)
+        T_K_guess = 0.0
+        p_kPa_guess = 0.0
+        state_key = self.translate_user_key_to_state(mode)
+        mf_column = matlab.double([[mf] for mf in mass_fracs])  # Spaltenvektor
 
 
-    def calc_transport_properties(self, state: ThermodynamicState):
-        """ Calculate transport properties of RefProp fluid at given state
+        result = eng.FluidCalc(
+            mf_column,
+            eos_choice,
+            refrigerants,
+            state_key,
+            float(var1),
+            float(var2),
+            float(T_K_guess),
+            float(p_kPa_guess),
+            nargout=1
+        )
 
-        Parameters:
-        -----------
-        :param ThermodynamicState state:
-            Current thermodynamic state
-        Return:
-        -------
-        :return TransportProperties props:
-            Instance of TransportProperties
-        """
-        # Get properties
 
-        props = TransportProperties(lam=None,
-                                    dyn_vis=None,
-                                    kin_vis=None,
-                                    pr=None,
-                                    cp=None,
-                                    cv=None,
-                                    beta=None,
-                                    sur_ten=None,
-                                    ace_fac=None,
-                                    state=state)
+        eng.quit()
 
-        assert self.props
-        props = self.props
-        #Return props
-        return props
+        p = self.get_field_safe(result, 'p_Pa')
+        T = self.get_field_safe(result, 'T_K')
+        h = self.get_field_safe(result, 'hh_Jkg')
+        s = self.get_field_safe(result, 'ss_JkgK')
+        d = self.get_field_safe(result, 'rho_kgm3')
+        q = self.get_field_safe(result, 'FracV_mass')
+        v = self.get_field_safe(result, 'v_spez')
+        u = h - p * v if h is not None and p is not None and v is not None else None
+
+        state = ThermodynamicState(
+            p=p,
+            T=T,
+            u=u,
+            h=h,
+            s=s,
+            d=d,
+            q=q
+        )
+        cp = self.get_field_safe(result, 'cp_JkgK')
+        dyn_vis = self.get_field_safe(result, 'vis_Pas')
+        lam = self.get_field_safe(result, 'lambda_WmK')
+        drho_dT = self.get_field_safe(result, 'drhokg_dT')
+
+        kin_vis = dyn_vis / d if dyn_vis is not None and d is not None else None
+        pr = cp * dyn_vis / lam if cp is not None and dyn_vis is not None and lam is not None else None
+        beta = - (1 / d) * drho_dT if d is not None and drho_dT is not None else None
+
+        sur_ten = None
+        ace_fac = None
+
+        props = TransportProperties(
+            lam=lam,
+            dyn_vis=dyn_vis,
+            kin_vis=kin_vis,
+            pr=pr,
+            cp=cp,
+            cv=self.get_field_safe(result, 'cv_JkgK'),
+            beta=beta,
+            sur_ten=sur_ten,
+            ace_fac=ace_fac,
+            state=state
+        )
+
+        return state, props
